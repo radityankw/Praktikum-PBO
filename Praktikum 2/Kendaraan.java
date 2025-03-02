1d925ed76281b4b4ebfa076aa990f28684ae02f2
radityankw <radityantari41@gmail.com>
2025-03-02 22:29:06 +0700

    Create Kendaraan.java

---
+++ Praktikum 2/Kendaraan.java
@@ -0,0 +1,40 @@
+/* Nama File : Kendaraan.java
+ * Deskripsi : berisi atribut dan method dalam class Kendaraan
+ * Pembuat   : Radityantari Kusuma Wardani
+ * Tanggal   : 27 Februari 2025
+ */
+
+public class Kendaraan {
+    
+    /***********ATRIBUT***********/
+    private String noPlat;
+    private String jenis;
+
+    /***********METHOD***********/
+    public Kendaraan() {
+        this.noPlat = "-";
+        this.jenis = "-";
+    }
+
+    public Kendaraan(String noPlat, String jenis) {
+        this.noPlat = noPlat;
+        this.jenis = jenis;
+    }
+
+    public String getNoPlat() {
+        return noPlat;
+    }
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    public void setNoPlat(String noPlat) {
+        this.noPlat = noPlat;
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+}
