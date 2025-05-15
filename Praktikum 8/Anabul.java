98db732252c470c13d3fe09cde819d73dabe0308
radityankw <radityantari41@gmail.com>
2025-05-15 19:39:41 +0700

    Upload Anabul.java

---
+++ Praktikum 8/Anabul.java
@@ -0,0 +1,34 @@
+/* File : Anabul.java
+ * Deskripsi : berisi atribut dan method untuk superclass Anabul
+ * NIM/Nama : 24060123130059/Radityantari Kusuma Wardani
+ */
+
+public class Anabul {
+    private String nama;
+
+    public Anabul(String nama) {
+        this.nama = nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    public String getNama(){
+        return nama;
+    }
+
+    public void gerak() {
+        
+    }
+
+    public void bersuara() {
+        
+    }
+
+    @Override
+    public String toString() {
+        return "Nama: " + nama;
+    }
+
+}
