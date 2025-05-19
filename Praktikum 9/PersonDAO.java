0d883f1daf92c91e9a89d66395c878c6058863c7
radityankw <radityantari41@gmail.com>
2025-05-19 23:16:01 +0700

    Upload PersonDAO.java

---
+++ Praktikum 9/PersonDAO.java
@@ -0,0 +1,8 @@
+/* File : PersonDAO.java
+ * Deskripsi : interface untuk person access object
+ * NIM/Nama : 24060123130059/Radityantari Kusuma Wardani
+ */
+
+public interface PersonDAO {
+    public void savePerson(Person p) throws Exception;
+}
